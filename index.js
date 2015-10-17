f5b47b0d778b5bf260554139b80506f0007feb25
Jamen Marz <jamen.marz@gmail.com>
2015-10-17 01:15:43 -0600

    Create basic server setup.

---
+++ index.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const http = require('http'),
+      https = require('https'),
+      lib = require('./lib'),
+      path = require('path'),
+      swig = require('swig'),
+      express = require('express'),
+
+app = express();
+
+// Templating engine
+app
+.engine('html', swig.renderFile)
+.set('view engine', 'html')
+.set('views', path.join(__dirname, 'views'));
+
+// Static files
+app
+.use('/static', express.static('/static', {dotfiles:'deny', maxAge:'6h'}));
+
+// Routes
+app
+.get(['/', '/home', '/index'], () => {})
+.get(['/me', '/you'], () => {})
+.get(['/settings', '/account'], () => {})
+.get('*', () => {});
+
+// Deploy.
+(function(listener, app, certs){
+  http.Server(app).listen(80, () => listener('HTTP'));
+  https.Server(certs, app).listen(443, () => listener('HTTPS'));
+})(function(server){
+  console.log(server + ' server is online.');
+}, app, {
+  // TODO: Get SSL certs.
+});
